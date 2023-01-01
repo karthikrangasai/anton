2ef124c1cbbb80b4f41fda44e19b1b6e83531f2d
Karthik Rangasai <karthikrangasai@gmail.com>
2023-01-02 02:19:36 +0530

    Refactor the matching code and add necessary tests.

---
+++ src/pyyamlconf/type_match.py
@@ -0,0 +1,65 @@
+import typing
+import warnings
+from typing import Any, Callable, Dict, List, Type, Union
+
+from pyyamlconf.constants import CONTAINER_TYPES, PRIMITIVE_TYPES
+
+
+def does_primitive_type_match(value: Any, parameter_type: Type) -> bool:
+    return isinstance(value, parameter_type)
+
+
+def does_union_type_match(value: Any, parameter_type: Type) -> bool:
+    union_types = typing.get_args(parameter_type)
+    return isinstance(value, union_types)
+
+
+def does_list_type_match(value: Any, parameter_type: Type) -> bool:
+    # NOTE: Assuming only List[primitive_type], List[Union[primitive_types, ...]]
+    # TODO: Consider cases like List[Tuple[...]], List[Dict[..., ...]], multiple nested stuff.
+
+    # List will have only one type. Hence indexing the first element.
+    list_elements_type = typing.get_args(parameter_type)[0]
+    container_obeys_type = isinstance(value, List)  # type: ignore
+    elements_obey_type = all([do_the_types_match(element, list_elements_type) for element in value])
+    return container_obeys_type and elements_obey_type
+
+
+def does_user_defined_class_match(value: Any, parameter_type: Type) -> bool:
+    # TODO: Check whether the yaml dict exactly matches the `__init__` params of the user-defined class.
+    #       Then we can do `user_defined_class(**yaml_provided_dict)`.
+    raise NotImplementedError()
+
+
+TYPE_TO_MATCHER_MAPPING: Dict[Any, Callable[[Any, Type], bool]] = {
+    list: does_list_type_match,
+    Union: does_union_type_match,
+}
+
+
+def do_the_types_match(value: Any, parameter_type: Type) -> bool:
+    actual_type = typing.get_origin(parameter_type)
+
+    if actual_type is not None:
+        # Now dealing with container types
+        # NOTE: Might need to check for being a subclass of `typing._GenericAlias`
+        if actual_type not in TYPE_TO_MATCHER_MAPPING:
+            warnings.warn(
+                f"Type matching code has been written for {actual_type} yet. Will assume the type passes for now.",  # type: ignore
+                RuntimeWarning,
+            )
+
+        type_checker_function = TYPE_TO_MATCHER_MAPPING[actual_type]
+        return type_checker_function(value, parameter_type)
+
+    # `parameter_type` is None: This could mean any primitve type, unsubscripted Union, user-defined class.
+    if parameter_type not in PRIMITIVE_TYPES:
+        if actual_type is Union:
+            # NOTE: For the case when parameter_type is typing.Union (not subscripted)
+            #       Assuming type `Any` -> Hence `True`
+            return True
+
+        # NOTE: Implement for case when parameter_type is a user-defined class
+        raise NotImplementedError()
+
+    return does_primitive_type_match(value, parameter_type)
--- src/pyyamlconf/yaml.py
+++ src/pyyamlconf/yaml.py
@@ -1,39 +1,15 @@
 import inspect
-import typing
 from dataclasses import MISSING as dataclass_missing_value
 from dataclasses import Field, dataclass
 from os import PathLike
 from pathlib import Path
-from typing import Any, Dict, Type, Union
+from typing import Any, Dict, Union
 
 import yaml
 
-from pyyamlconf.constants import CONTAINER_TYPES, PRIMITIVE_TYPES
+from pyyamlconf.type_match import do_the_types_match
 
 StrOrBytesPath = Union[str, Path, PathLike]
-
-
-def do_the_types_match(value: Any, parameter_type: Type) -> bool:
-    actual_type = typing.get_origin(parameter_type)
-
-    if actual_type is None:
-        if parameter_type in PRIMITIVE_TYPES:
-            return isinstance(value, parameter_type)
-
-        # NOTE: Implement for case when parameter_type is typing.Union (not subscripted)
-
-    # Now dealing with container types
-    # NOTE: Only solving for List type. List with single primitive (no Unions)
-    # NOTE: Might need to check for being a subclass of `typing._GenericAlias`
-    if actual_type is list:
-        list_elements_type = typing.get_args(parameter_type)
-        # NOTE: Assuming that the list elements are primitive types.
-
-        is_actually_container_instance = isinstance(value, actual_type)  # type: ignore
-        elements_obey_type = all([isinstance(element, list_elements_type) for element in value])
-        return is_actually_container_instance and elements_obey_type
-
-    return False
 
 
 def _yaml_conf_wrapper(
@@ -55,6 +31,7 @@ def _yaml_conf_wrapper(
     dataclass_fields: Dict[str, Field] = getattr(dataclass_cls, "__dataclass_fields__")
 
     # To ignore the self parameter, index from 1 to all.
+    # NOTE: Can extract default value here also. `y.default`
     actual_init_params = [(x, y.annotation) for x, y in inspect.signature(actual_init).parameters.items()][1:]
 
     def modified_init(self) -> None:
@@ -67,7 +44,7 @@ def _yaml_conf_wrapper(
                 raise exc
 
         pos_args = []
-        kw_args = {}
+        kw_args = {}  # NOTE: Can possibly use typing._get_defaults.
 
         for parameter_name, parameter_type in actual_init_params:
             if parameter_name not in conf_as_dict:
