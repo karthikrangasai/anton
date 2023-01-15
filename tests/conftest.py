f15920271b589643d51cf1b2190028f033eee1b4
Karthik Rangasai <karthikrangasai@gmail.com>
2023-01-15 17:22:08 +0530

    Refactor user defined dataclasses test case.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 import pytest
 
-import tests
-
 
 @pytest.fixture(scope="session")
 def base_dir_for_test_cases(tmp_path_factory: pytest.TempPathFactory) -> Path:
@@ -15,8 +13,3 @@ def base_dir_for_yaml_test_cases(base_dir_for_test_cases: Path) -> Path:
     YAML_TEST_CASES_DIR = base_dir_for_test_cases / "yaml"
     YAML_TEST_CASES_DIR.mkdir(parents=True, exist_ok=True)
     return YAML_TEST_CASES_DIR
-
-
-@pytest.fixture(scope="session")
-def simple_user_defined_class_yaml_file_path() -> Path:
-    return tests.SIMPLE_USER_DEFINED_CLASS_YAML_TEST_FILE
--- tests/test_simple_user_defined_class.py
+++ tests/test_simple_user_defined_class.py
@@ -3,14 +3,27 @@ from pathlib import Path
 
 from anton import yaml_conf
 
+TEST_CASE = """first_point:
+  x: 10
+  y: 10
+second_point:
+  x: 10
+  y: 10
+"""
 
-def test_simple_dict_yaml(simple_user_defined_class_yaml_file_path: Path) -> None:
+
+def test_simple_dict_yaml(base_dir_for_yaml_test_cases: Path) -> None:
+    TEST_CASE_PATH = base_dir_for_yaml_test_cases / "simple_user_defined_dataclasses.yaml"
+
+    with open(TEST_CASE_PATH, "w") as fp:
+        fp.write(TEST_CASE)
+
     @dataclass
     class Point:
         x: int
         y: int
 
-    @yaml_conf(conf_path=simple_user_defined_class_yaml_file_path)
+    @yaml_conf(conf_path=TEST_CASE_PATH)
     class SimpleDataclassConfiguration:
         first_point: Point
         second_point: Point = Point(20, 20)
